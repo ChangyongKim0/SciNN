41daaf55be36b1e1f2a5d244db634aa17c3069bb
ChangyongKim0 <changyong.kim.0@gmail.com>
2024-03-04 08:47:03 +0900

    G_RNN class created

---
+++ SciNN_G.py
@@ -0,0 +1,196 @@
+# %%
+import json
+import copy
+import random
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import math
+from collections import deque
+from itertools import chain
+
+
+matplotlib.use("Agg")
+
+
+class SciNN_G:
+    def __init__(self, length_list, eta_matrix, eta_BCM_matrix, input_index, output_index):
+        self.V = range(len(length_list))
+        self.n = length_list
+        self.eta = eta_matrix
+        self.eta_BCM = eta_BCM_matrix
+        self.i = input_index
+        self.o = output_index
+        self.h = [[0]*n_v for n_v in self.n]
+        self.h_state = [[0]*n_v for n_v in self.n]
+        self.W = [[[[0]*n_a
+                    for _ in range(n_b)] for n_a in self.n] for n_b in self.n]
+        self.E = [[(self.eta[b][a] > 0 or self.eta_BCM[b][a] > 0)
+                   for a in self.V] for b in self.V]
+        self.h_prev = [[0]*n_v for n_v in self.n]
+        self.W_prev = [[[[0]*n_a
+                         for _ in range(n_b)] for n_a in self.n] for n_b in self.n]
+        self.rho = 0.5
+        self.d = [self._getGraphDistance(
+            self.V, self.E, v, self.o) for v in self.V]
+        self.layer_depth = self._getGraphMinDepth(self.V, self.d)
+        self.initial_range = .5
+        self.max_range = 1
+        self.V_sorted = self._getGraphOrder(self.V, self.d)
+
+    def _getGraphMinDepth(self, V,  d):
+        min_depth = 0
+        for v in V:
+            min_depth = max(min_depth, d[v])
+        return min_depth + 1
+
+    def _getGraphDistance(self, V, E, start, end, error_value=-1):
+        queue = deque([start])
+        visited = {start: None}
+
+        while queue:
+            current_node = queue.popleft()
+            if current_node == end:
+                break
+            for v in V:
+                if E[v][current_node] and v not in visited:
+                    visited[v] = current_node
+                    queue.append(v)
+                    # print(start, end, v, current_node, E[v][current_node], visited)
+
+        path = []
+
+        while end != None:
+            path.append(end)
+            end = visited[end]
+            # print(end, path)
+
+        return len(path)-1 if path else error_value
+
+    # def _topologicalSortUtil(self, V, E, v, visited, stack):
+    #     visited[v] = True
+    #     for w in V:
+    #         if not visited[w] and E[w][v]:
+    #             self._topologicalSortUtil(V, E, w, visited, stack)
+    #     stack.append(v)
+
+    # def _getTopOrder(self, V, E):
+    #     visited = [False]*len(V)
+    #     stack = []
+    #     for v in V:
+    #         if not visited[v]:
+    #             self._topologicalSortUtil(V, E, v, visited, stack)
+    #     return stack[::-1]
+
+    def _getGraphOrder(self, V, d):
+        temp = [[] for _ in range(self.layer_depth)]
+        for v in V:
+            temp[d[v]].append(v)
+            # print(temp)
+        return list(chain(*temp))[::-1]
+
+    def _deleteDiagonal(self):
+        for v in self.V:
+            for i in range(len(self.h[v])):
+                self.W[v][v][i][i] = 0
+
+    def _getInternalSynapseNo(self, sparsity, N):
+        return sparsity*math.pow(N, 4/3)
+
+    def _getInternalSynapseDensity(self, sparsity, N):
+        return self._getInternalSynapseNo(sparsity, N)/N/N
+
+    def _getDirectionalSynapseDensity(self, sparsity, N1, N2):
+        return (self._getInternalSynapseNo(sparsity, N1+N2) - self._getInternalSynapseNo(sparsity, N1) - self._getInternalSynapseNo(sparsity, N2))/N1/N2/2
+
+    def _getPreLayer(self, b, V, E):
+        pre_layer_list_of_b = []
+        for a in V:
+            if E[b][a] and b != a:
+                pre_layer_list_of_b.append(a)
+        return pre_layer_list_of_b
+
+    def initializeWeight(self, sparsity=2.27):
+        for b in self.V:
+            for a in self.V:
+                if b == a:
+                    synapse_density = self._getInternalSynapseDensity(
+                        sparsity, len(self.h[b]))
+                elif self.E[b][a]:
+                    synapse_density = self._getDirectionalSynapseDensity(
+                        sparsity, len(self.h[b]), len(self.h[a]))
+                if self.E[b][a]:
+                    for j in range(len(self.h[a])):
+                        if self.h_state[a][j] == 0:
+                            h_state_a_j = 1 if random.random() < self.rho else -1
+                        else:
+                            h_state_a_j = self.h_state[a][j]
+                        for i in range(len(self.h[b])):
+                            if random.random() < synapse_density:
+                                self.h_state[a][j] = h_state_a_j
+                                self.W[b][a][i][j] = random.random(
+                                )*self.initial_range*h_state_a_j
+        self._deleteDiagonal()
+
+    def _makeDataFrame(self, V, h, W, h_prev):
+        temp_data = [{'from': 't', 'to': 't', 'weight': None}]
+        temp_data = [{'from': 't', 'to': 't-1', 'weight': None}]
+        for v in V:
+            for i in range(len(h[v])):
+                temp_data.append(
+                    {'from': f'{v}-{i}', 'to': 't-1', 'weight':  self.max_range/2, 'strength': h_prev[v][i]})
+                temp_data.append(
+                    {'from': f'{v}-{i}', 'to': 't', 'weight':  self.max_range/2, 'strength': h[v][i]})
+                temp_data.append(
+                    {'from': 't', 'to': f'{v}-{i}', 'weight':  self.max_range/2, 'strength': h[v][i]})
+        for b in V:
+            for a in V:
+                for i in range(len(h[b])):
+                    for j in range(len(h[a])):
+                        temp_data.append({'from': f'{a}-{j}', 'to': f'{b}-{i}', 'weight': abs(
+                            W[b][a][i][j]), 'strength': W[b][a][i][j]*h_prev[a][j]})
+        return pd.DataFrame(temp_data)
+
+    def drawHeatmap(self):
+        # plt.close()
+        data = self._makeDataFrame(self.V, self.h, self.W, self.h_prev)
+        g = sns.relplot(x="from", y="to",
+                        hue="strength", size="weight",
+                        hue_norm=(-self.max_range, self.max_range), size_norm=(0, self.max_range),
+                        palette="coolwarm",
+                        sizes=(0, 3000/(sum([len(self.h[l])
+                               for l in range(len(self.h))]))),
+                        marker="s", linewidth=0, legend=False,
+                        aspect=1.25, data=data)
+        g.ax.invert_yaxis()
+        ghost = g.ax.scatter([], [], c=[], vmin=-self.max_range,
+                             vmax=self.max_range, cmap="coolwarm")
+        g.fig.colorbar(ghost)
+        return g
+
+    def saveHeatmap(self, file_name):
+        g = self.drawHeatmap()
+        g.savefig(file_name)
+        plt.close('all')
+        plt.clf()
+
+    def saveCurrent(self, file_name):
+        to_save = {"h": self.h, "W": self.W, "h_prev": self.h_prev,
+                   "W_prev": self.W_prev}
+        with open("data/"+file_name+".json", "w") as f:
+            json.dump(to_save, f)
+
+    def loadData(self, file_name):
+        with open("data/"+file_name+".json", "r") as f:
+            to_load = json.load(f)
+        self.h, self.W, self.h_prev, self.W_prev = to_load[
+            "h"], to_load["W"], to_load["h_prev"], to_load["W_prev"]
+
+
+# a = SciNN(3)
+# a.addLayer(4)
+# a.initializeWeight()
+
+# %%
---
+++ SciNN_G_RNN.py
@@ -0,0 +1,165 @@
+import SciNN_G as sg
+import copy
+import numpy as np
+import math
+import random
+from collections import deque
+
+
+class SciNN_G_RNN(sg.SciNN_G):
+    def __init__(self, length_list, eta_matrix, eta_BCM_matrix, input_index, output_index):
+        super().__init__(length_list, eta_matrix, eta_BCM_matrix, input_index, output_index)
+        self.delta = .01
+        self.epsilon = .01
+        self.lr = 0.05
+        self.theta = [[0]*n_v for n_v in self.n]
+        self.actFn = math.tanh
+        self.actFnDer = lambda y: 1 - y**2
+        self.feedSelf = lambda output: output
+        # objects below will be used in BP only
+        # (i)-th index means the value at time (t-i)
+        self.hs = [copy.deepcopy(self.h) for _ in range(self.layer_depth+1)]
+        self.Ws = [copy.deepcopy(self.W) for _ in range(self.layer_depth+1)]
+        self.loss = -1
+        self.target_vector = [0]*self.n[self.o]
+        self.max_tanh_value = 0.99
+
+    def _deleteDiagonal(self):
+        super()._deleteDiagonal()
+        for d in range(self.layer_depth):
+            for v in self.V:
+                for i in range(len(self.h[v])):
+                    self.Ws[d][v][v][i][i] = 0
+
+    def initializeWeight(self, sparsity=2.27):
+        super().initializeWeight(sparsity)
+        self.hs = [copy.deepcopy(self.h) for _ in range(self.layer_depth+1)]
+        self.Ws = [copy.deepcopy(self.W) for _ in range(self.layer_depth+1)]
+
+    def decodeWeightOfBackprop(self):
+        self.W = [[[[np.arctanh(self.W[b][a][i][j]) for j in range(
+            self.n[a])] for i in range(self.n[b])] for a in self.V] for b in self.V]
+        self.W_prev = self.W
+        self.hs = [copy.deepcopy(self.h) for _ in range(self.layer_depth+1)]
+        self.Ws = [copy.deepcopy(self.W) for _ in range(self.layer_depth+1)]
+
+    def setFeedSelf(self, feedSelf):
+        self.feedSelf = feedSelf
+
+    def setLossFunction(self, fn_type):
+        def _softmax(output):
+            output_sum = sum([np.exp(output[i]) for i in range(len(output))])
+            return [np.exp(output[i])/output_sum for i in range(len(output))]
+        if fn_type == "cross entropy":
+            self.lossFn = lambda output, target: -1 * \
+                sum([target[i]*np.log(_softmax(output)[i])
+                    for i in range(len(output))])
+            self.lossFnDer = lambda output, target: [-1*target[i]*(
+                1-_softmax(output)[i]) for i in range(len(output))]
+        elif fn_type == "mse":
+            self.lossFn = lambda output, target: sum(
+                [(output[i]-target[i])**2 for i in range(len(output))])/len(output)
+            self.lossFnDer = lambda output, target: [
+                2*(output[i] - target[i])/len(output) for i in range(len(output))]
+        else:
+            0
+
+    def propagateBP(self, input_vector, target_vector):
+        self.propagateBPConsideringOutput(input_vector, lambda: target_vector)
+
+    def propagateBPConsideringOutput(self, input_vector, getTargetVectorFromOutput):
+        if len(input_vector) != len(self.h[self.i])-len(self.feedSelf(self.h[self.o])):
+            print("ERROR: wrong input length")
+            return False
+        else:
+            # at t, forward
+            for b in self.V_sorted:
+                self.h_prev[b] = copy.deepcopy(self.h[b])
+                if b != self.i:
+                    pre_of_b = self._getPreLayer(b, self.V, self.E)
+                    self.hs[self.d[b]][b] = [self.actFn(sum([np.matmul(
+                        self.Ws[self.d[b]+1][b][a], self.hs[self.d[b]+1][a])[i] for a in pre_of_b])) for i in range(self.n[b])]
+                self.h[b] = copy.deepcopy(self.hs[self.d[b]][b])
+            # at t, backward
+            dL_dh = [[0]*n_v for n_v in self.n]
+            self.target_vector = getTargetVectorFromOutput(self.h[self.o])
+            self.loss = self.lossFn(self.hs[0][self.o], self.target_vector)
+            for a in self.V_sorted[::-1]:
+                if a == self.o:
+                    dL_dh[a] = self.lossFnDer(
+                        self.hs[0][self.o], self.target_vector)
+                else:
+                    for b in self.V:
+                        if self.E[b][a] and self.d[b]+1 == self.d[a]:
+                            for j in range(self.n[a]):
+                                dL_dh[a][j] += sum([self.Ws[self.d[a]][b][a][i][j]*self.actFnDer(
+                                    self.hs[self.d[b]][b][i])*dL_dh[b][i] for i in range(self.n[b])])
+            for a in self.V:
+                for b in self.V:
+                    if self.E[b][a]:
+                        self.W_prev[b][a] = copy.deepcopy(self.W[b][a])
+                        for i in range(self.n[b]):
+                            for j in range(self.n[a]):
+                                self.Ws[self.d[b]
+                                        ][b][a][i][j] = self.Ws[self.d[b]+1][b][a][i][j]-self.eta[b][a] * \
+                                    self.hs[self.d[b]+1][a][j] * \
+                                    self.actFnDer(
+                                        self.hs[self.d[b]][b][i])*dL_dh[b][i]
+                                if self.Ws[self.d[b]][b][a][i][j] * self.h_state[a][j] < 0:
+                                    self.Ws[self.d[b]][b][a][i][j] = 0
+                                if self.Ws[self.d[b]][b][a][i][j] * self.h_state[a][j] >= self.max_tanh_value:
+                                    self.Ws[self.d[b]][b][a][i][j] = self.max_tanh_value * \
+                                        self.h_state[a][j]
+
+                        self.W[b][a] = copy.deepcopy(self.Ws[self.d[b]][b][a])
+            self._deleteDiagonal()
+            # at t+1, backward
+            self.Ws.insert(0, [[[[0]*n_a
+                                 for _ in range(n_b)] for n_a in self.n] for n_b in self.n])
+            self.Ws.pop(-1)
+            # at t+1, forward
+            self.hs.insert(0, [[0]*n_v for n_v in self.n])
+            self.hs[0][self.i] = [
+                *input_vector, *self.feedSelf(self.hs[1][self.o])]
+            self.hs.pop(-1)
+
+    def propagateBCM(self, input_vector):
+        if len(input_vector) != len(self.h[self.i])-len(self.feedSelf(self.h[self.o])):
+            print("ERROR: wrong input length")
+            return False
+        else:
+            self.h_prev = copy.deepcopy(self.h)
+            self.W_prev = copy.deepcopy(self.W)
+            for b in self.V:
+                if b == self.i:
+                    self.h[b] = [*input_vector, *self.h_prev[self.o]]
+                else:
+                    pre_of_b = self._getPreLayer(b, self.V, self.E)
+                    v_b = sum([np.matmul(self.W_prev[b][a], self.h_prev[a])
+                              for a in pre_of_b])
+                    # tilde_w_b = sum(
+                    #     [np.matmul(self.W_prev[b][a], [1]*len(self.h_prev[a])) for a in pre_of_b])
+                    for i in range(len(self.h[b])):
+                        self.h[b][i] = sum([sum([max(0, math.tanh(
+                            self.h_prev[a][j]*math.tanh(self.W_prev[b][a][i][j]))) for j in range(self.n[a])]) for a in pre_of_b])
+                        try:
+                            self.theta[b][i] = (
+                                1-self.delta)*self.theta[b][i]+self.delta*math.pow(v_b[i], 3)
+                        except:
+                            print(self.theta[b][i], v_b[i])
+                        for a in pre_of_b:
+                            phi = v_b[i] * (v_b[i] - self.theta[b][i])
+                            for j in range(len(self.h[a])):
+                                if self.W_prev[b][a][i][j] != 0:
+                                    self.W[b][a][i][j] = (
+                                        1 if self.W_prev[b][a][i][j] > 0 else -1)*max(0, abs(self.W_prev[b][a][i][j])+self.eta_BCM[b][a]*phi*self.h[a][j])
+                                else:
+                                    if self.h_state[a][j] == 0:
+                                        r = random.random()
+                                        if r < self.epsilon*self.rho:
+                                            self.h_state[a][j] = 1
+                                        elif r < self.epsilon:
+                                            self.h_state[a][j] = -1
+                                    self.W[b][a][i][j] = self.h_state[a][j]*max(
+                                        0, self.eta_BCM[b][a]*phi*self.h[a][j])
+        self._deleteDiagonal()
